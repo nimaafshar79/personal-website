e06d78ecd83703290b95b42ddaae5f21f0dbade2
nimaa <nimaafshar79@gmail.com>
2016-03-24 13:46:51 +0430

    last commit

---
+++ database/migrations/2016_03_23_192114_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("title");
+            $table->string("body" , 1000);
+            $table->integer("user_id")->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
